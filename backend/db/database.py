a7238bbdf0efa716b938f0985c959c1dd538934b
Bhagyashree Sethiya <bhagyashreesethiya@gmail.com>
2026-07-06 08:40:19 +0530

    Initial Setup with FastAPI and PostgreSQL

--- backend/db/database.py
+++ backend/db/database.py
@@ -4,7 +4,6 @@ from core.config import settings
 
 # 1. Direct PostgreSQL URL read krna env. variables s
 engine = create_engine(settings.DATABASE_URL)
-
 #2. Session Setup
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
