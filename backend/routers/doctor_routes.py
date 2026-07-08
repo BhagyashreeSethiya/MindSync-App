439df04b6182965afcbe5bd9bb81864bf28ab518
Bhagyashree Sethiya <bhagyashreesethiya@gmail.com>
2026-07-08 07:14:15 +0530

    added endpoint for auto-suggesting search patients dropdown

--- backend/routers/doctor_routes.py
+++ backend/routers/doctor_routes.py
@@ -36,7 +36,7 @@ async def get_mood_history(db: Session = Depends(get_db), current_user: User = D
 
 
 #2. HTTP Polling endpoint (Active alerts k liye)
-#React Dashboard ise har 10-15 seconds m hit krega
+#React Dashboard ise har 10 seconds m hit krega
 @router.get("/active-alerts", response_model=List[CaretakerAlertResponse])
 async def get_active_alerts(db:Session = Depends(get_db), current_user: User = Depends(require_role("care_taker"))):
    
@@ -46,20 +46,21 @@ async def get_active_alerts(db:Session = Depends(get_db), current_user: User = D
 
     return [
         {
-            "id":alert.id,
-            "user_id":alert.user_id,
-            "emotion": alert.emotion_detected,
-            "message": alert.user_message,
-            "timestamp": alert.timestamp
+            "id":log.id,
+            "user_id":log.user_id,
+            "patient_name": log.owner.name, # using FK relationship
+            "emotion": log.emotion_detected,
+            "message": log.user_message,
+            "timestamp": log.timestamp
         }
-        for alert in alerts
+        for log in alerts
     ]
 
 
 #3. Specific Patient k history dekhne k liye
 @router.get("/patient/{patient_id}", response_model=List[CaretakerLogResponse] )
 async def get_patient_mood_history(
-    patient_id:int, #URL s patient k id aajegi
+    patient_id:int, #URL s patient ka name aajega
     db: Session = Depends(get_db),
     current_user: User = Depends(require_role("care_taker"))
 ):
@@ -89,4 +90,31 @@ async def get_patient_mood_history(
         }
         for log in logs
     ]
+
+
+@router.get("/search-patients")
+async def search_patients(
+    q:str,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(require_role("care_taker"))
+):
+    if not q or len(q) < 2:
+        return [] #Agar 2 letter s kam type kiya h toh kuch mat bhejo
+    
+    patients = db.query(User).filter(
+        User.name.ilike(f"%{q}%"),
+        User.role == "patient"
+    ).all()
+
+    #dropdown k liye list kr rhe h jisme email bhi ho
+    return [
+        {
+            "id":p.id,
+            "name":p.name,
+            "email":p.email
+        }
+        for p in patients
+    ]
+
+
 
