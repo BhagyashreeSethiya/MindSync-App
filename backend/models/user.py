4b14872331407a67226ac29f382e25600d13d442
Bhagyashree Sethiya <bhagyashreesethiya@gmail.com>
2026-07-23 16:03:57 +0530

    feat: add caretaker-patient self-referential relationship in user model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from db.database import Base
@@ -17,6 +17,16 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     is_verified = Column(Boolean, default = False)
 
+    # Foreign Key: Jo dusre User (Caretaker) li ID store karega
+    caretaker_id = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"), nullable = True )
+
+    # Self-Referential Relationships (Doctor aur Patient ko link karne k liye)
+
+    #1. Patient k side s apna Doctor dekhne k liye:
+    caretaker = relationship("User", remote_side=[id], back_populates="patients", foreign_keys=[caretaker_id])
+
+    #2. Doctor k side s apne saare patients ki list dekhne k liye:
+    patients = relationship("User", back_populates="caretaker", foreign_keys=[caretaker_id])
     #Relationship link to MoodLogs (one-to-many)
     #cascade="all,delete" ka mtlb agr user delete hua toh uske logs bhi ud jayenge
     logs = relationship("MoodLog", back_populates="owner", cascade="all, delete-orphan")
