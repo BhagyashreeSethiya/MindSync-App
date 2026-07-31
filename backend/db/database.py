e713f9a572f621daa4f072d75b946be8fd20ab55
Bhagyashree Sethiya <bhagyashreesethiya@gmail.com>
2026-07-31 22:03:13 +0530

    fix: added pool_pre_ping to fix db connection drop

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from core.config import settings
 
 # 1. Direct PostgreSQL URL read krna env. variables s
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 #2. Session Setup
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
